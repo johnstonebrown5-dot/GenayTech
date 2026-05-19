ee732c1bfb917e98bc64e0d27224736b9cd252be
johnstonebrown5-dot <johnstonebrown5@gmail.com>
2026-05-19 19:34:55 +0300

    Marks

--- backend/communications/migrations/0019_fix_duplicate_check_constraints.py
+++ backend/communications/migrations/0019_fix_duplicate_check_constraints.py
@@ -13,19 +13,19 @@ class Migration(migrations.Migration):
         # Remove old check constraints that might exist
         migrations.RunSQL(
             "ALTER TABLE communications_arrearsmessagecampaign DROP CONSTRAINT IF EXISTS communications_arrearsme_email_failed_f72719ac_check",
-            reverse_sql=migrations.RunSQL.NOOP,
+            reverse_sql="",
         ),
         migrations.RunSQL(
             "ALTER TABLE communications_arrearsmessagecampaign DROP CONSTRAINT IF EXISTS communications_arrearsme_email_sent_8c7f5e2c_check",
-            reverse_sql=migrations.RunSQL.NOOP,
+            reverse_sql="",
         ),
         migrations.RunSQL(
             "ALTER TABLE communications_arrearsmessagecampaign DROP CONSTRAINT IF EXISTS communications_arrearsme_sms_failed_3e8f5a2c_check",
-            reverse_sql=migrations.RunSQL.NOOP,
+            reverse_sql="",
         ),
         migrations.RunSQL(
             "ALTER TABLE communications_arrearsmessagecampaign DROP CONSTRAINT IF EXISTS communications_arrearsme_sms_sent_2d8f5a2c_check",
-            reverse_sql=migrations.RunSQL.NOOP,
+            reverse_sql="",
         ),
         # Now alter the fields to PositiveIntegerField with new constraints
         migrations.AlterField(
