24fba82ab2466b22fb022cb37570823a21a7bf3e
johnstonebrown5-dot <johnstonebrown5@gmail.com>
2026-05-22 12:16:06 +0300

    Backend from CODEX

---
+++ backend/communications/migrations/0019_merge_0017_alter_arrearsmessagecampaign_email_failed_and_more_0018_rename_comm_del_school_created_idx.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('communications', '0017_alter_arrearsmessagecampaign_email_failed_and_more'),
+        ('communications', '0018_rename_comm_del_school_created_idx_communicati_school__d4dbf1_idx_and_more'),
+    ]
+
+    operations = []
